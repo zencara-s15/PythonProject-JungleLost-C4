c7a171b69f1649a95c3190ecf438bd9910c189e3
SEREYPUTHIREACH.CHIN <SEREYPUTHIREACH.CHIN@pnc.lan>
2023-10-21 11:08:21 +0700

    #1 update window screen height and width

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+from tkinter import *
+from PIL import Image, ImageTk
+import time
+
+# varaible----
+WIDTH = 1400
+HEIGHT = 700
+xMove = 10
+window = Tk()
+
+canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
+window.mainloop()
